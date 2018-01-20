a7146cdeba6ad57aa16765b28b4d71fc9a955f84
Maxwell Lasky <mlasky46@gmail.com>
2018-01-19 20:31:45 -0700

    config will automatically upload to match latest release

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
               ).browser_download_url;
               break;
           }
+          option.name = parsed.name;
           updated.push(option);
         });
         this.setState({ downloadOptions: updated });
